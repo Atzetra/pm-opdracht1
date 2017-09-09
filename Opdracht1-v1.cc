4824ca8fb06f532f56f8dee75978a82fd3dbe7c9
Atzetra <e.sastrokarijo@gmail.com>
2017-09-09 02:20:36 +0200

    Toevoegen inlezen geboortedatum en daum ophalen met cmake

--- Opdracht1-v1.cc
+++ Opdracht1-v1.cc
@@ -2,11 +2,38 @@
 // Programmeermethoden 2017-2018
 
 #include <iostream>
+#include <ctime>
+
 using namespace std;
 
 int main(){
 
-    cout << "Hello World" << endl;
+    int geboortejaar, geboortemaand, geboortedag;
+
+    cin >> geboortejaar >> geboortemaand >> geboortedag;
+
+    // Testregel om uit te vindeb of de variabelen goed binnenkomen
+    cout << geboortejaar << " " << geboortemaand << " " << geboortedag;
+
+    if (geboortemaand > 12 || geboortemaand < 1){
+        return 1;
+    }
+
+    // Ctime om datum uit te vinden
+
+    int hdag, hmaand, hjaar;
+
+    // Hulpvariabelen voor ctime
+    tm s;
+    time_t t;
+
+    // Calculatie datum
+    time (&t);
+    s = ` localtime (&t);
+    hdag = s.tm_mday;
+    hmaand = s.tm_mon + 1;      // Januari is 0, dus + 1
+    hjaar = s.tm_year + 1900;   // Telt vanaf 1900, dus + 1900
+    
     return 0;
 
 
